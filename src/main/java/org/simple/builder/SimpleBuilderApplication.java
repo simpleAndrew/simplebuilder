2a83e9893bd4593c7b27200072b2fec4cda573d3
Andrew Shchyolok <andrew.shchyolok@strevus.com>
2016-12-26 16:41:49 +0100

    Initial structure with spring dependencies

---
+++ src/main/java/org/simple/builder/SimpleBuilderApplication.java
@@ -0,0 +1,12 @@
+package org.simple.builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBuilderApplication.class, args);
+	}
+}
---
+++ src/test/java/org/simple/builder/SimpleBuilderApplicationTests.java
@@ -0,0 +1,16 @@
+package org.simple.builder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleBuilderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
