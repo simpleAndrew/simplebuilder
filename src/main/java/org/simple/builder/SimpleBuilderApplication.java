ed5bd232e130103dea7c8df83306339d11b7984c
Andrew Shchyolok <andrew.shchyolok@strevus.com>
2017-02-15 22:58:12 +0100

    add support of hateous link building based ob resource and dedicated controller

--- src/main/java/org/simple/builder/SimpleBuilderApplication.java
+++ src/main/java/org/simple/builder/SimpleBuilderApplication.java
@@ -2,8 +2,10 @@ package org.simple.builder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableEntityLinks;
 
 @SpringBootApplication
+@EnableEntityLinks
 public class SimpleBuilderApplication {
 
 	public static void main(String[] args) {
